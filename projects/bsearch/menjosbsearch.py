f9882c53b37e62dcb6aebeeadc6feb5e93c989ff
Selina <selina.jones@bison.howard.edu>
2013-11-18 17:28:29 -0500

    Update menjosbsearch.py
    Added Selina Jones bsearch code

--- projects/bsearch/menjosbsearch.py
+++ projects/bsearch/menjosbsearch.py
@@ -21,3 +21,29 @@ def bsearch( alist , element):
 
 	return -1
 #Maimuna Ahmed's code ends hur
+#Selina Jones bsearch code
+mylist=[0,6,98,1,4,88,33,91,43,90,78]
+mylist.sort()
+
+
+
+def bsearch(mylist,x):
+    size=len(mylist)
+    low=0
+    high=size
+    if mylist==[]:
+        return mylist
+    if high==size:
+        high =len(mylist)-1
+    while low<=high:
+        mid = (low+high)/2
+        midval=mylist[mid]
+        if midval<x:
+            low=mid+1
+        elif midval>x:
+            high=mid
+        else:
+            return mid
+    return -1
+            
+bsearch([0,6,98,1,4,88,33,91,43,90,78],0)
